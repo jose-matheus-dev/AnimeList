bfb40b48cc032a6c53ae27a589712d091086b48c
José Matheus <jose-matheus-dev@outlook.com>
2025-01-02 10:57:44 -0300

    feat(home): add dynamic hero banner with anime themes

---
+++ src/utils/AnimeList.ts
@@ -0,0 +1,37 @@
+const animes: AnimeProps[] = [
+  {
+    id: 1,
+    name: 'Jujutsu Kaisen',
+    arc: 'Shibuya Incident',
+    description:
+      'O arco Shibuya Incident ocorre em 31 de outubro, quando um véu amaldiçoado isola Shibuya. Feiticeiros enfrentam espíritos e maldições em batalhas caóticas e intensas. Tragédias e reviravoltas mudam o curso da história de forma decisiva.',
+    cover:
+      'https://m.media-amazon.com/images/M/MV5BZDZkMGQzOWItZGMzZC00MDVhLWI3NTYtN2ZlMDA4ODliMzJhXkEyXkFqcGc@._V1_.jpg',
+    colors: ['#9239e5', '#be91e7'],
+    accent: '#be91e7',
+  },
+  {
+    id: 2,
+    name: 'Demon Slayer',
+    arc: 'Tanjiro Kamado, Unwavering Resolve',
+    description:
+      'Tanjiro luta para curar sua irmã Nezuko e se junta ao Corpo de Caçadores de Demônios, enfrentando demônios poderosos, com sua convicção inabalável.',
+    cover:
+      'https://m.media-amazon.com/images/M/MV5BNmQ5Zjg2ZTYtMGZmNC00M2Y3LTgwZGQtYmQ3NWI5MDdhZWNjXkEyXkFqcGc@._V1_.jpg',
+    colors: ['#1b352f', '#5ed6cb'],
+    accent: '#5ed6cb',
+  },
+  {
+    id: 3,
+    name: 'Spy &times; Family',
+    arc: 'Operation Strix',
+    description:
+      'A primeira temporada de Spy &times; Family, segue a história de um espião, uma assassina e uma criança psíquica que formam uma família falsa para uma missão secreta',
+    cover:
+      'https://m.media-amazon.com/images/M/MV5BZDkwNjc0NWEtNzJlOC00N2YwLTk4MjktZGFlZDE2Y2QzOWI0XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    colors: ['#b4effd', '#1b3b67'],
+    accent: '#b4effd',
+  },
+];
+
+export { animes };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+type AnimeProps = {
+  id: number;
+  name: string;
+  arc: string;
+  description: string;
+  cover: string;
+  colors: string[];
+  accent: string;
+};
