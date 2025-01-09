4535590cd81aeecd5c90fd8936725218fb7db188
José Matheus <jose-matheus-dev@outlook.com>
2025-01-09 10:31:16 -0300

    feat(layout): add ResponsiveGuard component to prevent portrait mode below 3:2 ratio

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,3 +2,4 @@ export * from './Header.tsx';
 export * from './Main.tsx';
 export * from './Aside.tsx';
 export * from './Background.tsx';
+export * from './ResponsiveGuard.tsx';
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 
-import { AppStyles, NormalizeStyles } from '@/components/theme';
 import App from '@/App';
+import { ResponsiveGuard } from '@/components/layout';
+import { AppStyles, NormalizeStyles } from '@/components/theme';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NormalizeStyles />
     <AppStyles />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ResponsiveGuard>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ResponsiveGuard>
   </React.StrictMode>
 );
